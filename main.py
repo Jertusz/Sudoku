990fbd195c4d272016601a17b7a12685d7a93f61
Jedrzej Szadejko <jendziu@gmail.com>
2020-08-30 14:51:50 +0200

    Created solve function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ class Node:
     def __init__(self, row, col):
         self.row = row
         self.col = col
-        self.filled = 0 
+        self.value = 0 
         
 
     def get_pos(self):
@@ -11,15 +11,15 @@ class Node:
 
 
     def is_filled(self):
-        return self.filled != 0
+        return self.value != 0
 
 
-    def fill(self, number):
-        self.filled = number
+    def set_value(self, number):
+        self.value = number
 
 
-    def get_number(self):
-        return self.filled
+    def get_value(self):
+        return self.value
 
 
 # class Cube:
@@ -73,11 +73,98 @@ class Grid:
     def print_grid(self):   # for debug purposes only
         for row in self.get_nodes():
             for node in row:
-                print(node.get_number(), end="\t")
+                print(node.get_value(), end="\t")
             print("\n")
             print("\n")
+
+
+    def check_grid(self):
+        nodes = self.get_nodes()
+        for row in nodes:
+            for col in range(9):
+                if not nodes[row][col].is_filled():
+                    return False
+
+        return True
+
+
+    def get_row_values(self, row):
+        vals = []
+        for node in self.get_nodes()[row]:
+            vals.append(node.get_value())
+
+        return vals
+
+
+    def get_col_values(self, col):
+        vals = []
+        for i in range(9):
+            node = self.get_nodes()[i][col] 
+            vals.append(node.get_value())
+
+        return vals
+
+
+    def get_square(self, end_col, end_row):
+        start_col = end_col - 3
+        start_row = end_row - 3
+        square = []
+        for i in range(start_row, end_row):
+            square.append(self.get_nodes()[i][start_col:end_col])
+        return square
+
+
+    def solve(self):
+        for i in range(0, 81):
+            row = i//9
+            col = i%9
+            if self.get_nodes()[row][col] == 0:
+                for value in range(1, 10):
+                    if value not in self.get_row_values(row):
+                        if value not in self.get_col_values(col):
+                            square = []
+                            if row < 3:
+                                if col < 3:
+                                    square = self.get_square(3, 3)
+                                elif col < 6:
+                                    square = self.get_square(6, 3)
+                                else:
+                                    square = self.get_square(9, 3)
+                            elif row < 6:
+                                if col < 3:
+                                    square = self.get_square(3, 6)
+                                elif col < 6:
+                                    square = self.get_square(6, 6)
+                                else:
+                                    square = self.get_square(9, 6)
+                            else:
+                                if col < 3:
+                                    square = self.get_square(3, 9)
+                                elif col < 6:
+                                    square = self.get_square(6, 9)
+                                else:
+                                    square = self.get_square(9, 9)
+                            square_vals = []
+                            for row in square:
+                                for node in row:
+                                    square_vals.append(node.get_value())
+                            if value not in square_vals:
+                                self.get_nodes()[row][col].set_value(value)
+                                if self.check_grid():
+                                    return True
+                            else:
+                                if self.solve():
+                                    return True
+                break
+        print("backtrack")
+        self.print_grid()
+        self.get_nodes()[row][col].set_value(0)
+
+
+
 
 
 
 g = Grid()
 g.print_grid()
+g.solve()
